7e01a608156bb361c20593acadbacebbec40307a
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-02-21 18:30:33 +0000

    Update twoup.js

--- commands/Economy/Games/twoup.js
+++ commands/Economy/Games/twoup.js
@@ -1,13 +1,13 @@
 exports.run = async (client, msg, [bet]) => {
     let rolls = [];
     for (var z = 0; z < 7; z++) {
-        var x = (Math.random() > .5) ? "heads" : "tails";
+        var x = (Math.random() > .5) ? "cara" : "coroa";
         rolls.push(x);
     }
 
     let fact = [];
     for (var y = 0; y < 7; y++) {
-        if (rolls[y] === rolls[y + 1] && rolls[y] === "heads") { var result = ["won", 1.4]; break; }
+        if (rolls[y] === rolls[y + 1] && rolls[y] === "cara") { var result = ["won", 1.4]; break; }
         else if (rolls[y] !== rolls[1]) { fact.push(true); }
         else if (y === 6) { var result = fact.includes(false) ? ["lost", -1] : ["won", 2]; } 
         else { fact.push(false); }
@@ -16,7 +16,7 @@ exports.run = async (client, msg, [bet]) => {
     client.funcs.transactions(msg, {credit: [bet, "*", result[1]]}, function(data) {
         if (data.valid === false) { return; }
 
-        msg.channel.send(`**Coins:** ${rolls.join(", ")}\nYou have ${result[0]} ${Math.abs(data.earnings)} credits.`); 
+        msg.channel.send(`**Moedas:** ${rolls.join(", ")}\nTu tens ${result[0]} ${Math.abs(data.earnings)} créditos.`); 
     });
 };
 
@@ -30,8 +30,8 @@ exports.conf = {
 
 exports.help = {
     name: "twoup",
-    description: "Bet on coin flips. Get two heads in a row and win or hope for all five odds!",
+    description: "Aposta no cara-coroa. Obtém duas caras seguidas e ganha... Ou reza para que saiam todos ímpares!",
     usage: "[bet:int]",
-    extendedHelp: "Two-up is a traditional Australian gambling game, involving a designated 'spinner' throwing two coins into the air. Players bet on whether the coins will fall with both heads up, both tails up, or with one head and one tail up (known as 'odds'). It is traditionally played on Anzac Day in pubs and clubs throughout Australia, in part to mark a shared experience with Diggers through the ages.",
+    extendedHelp: "Two-up é um jogo típico Autraliano, envolvendo um designado 'spinner' atira duas moedas ao ar. Os Players apostam no que quiserem em que as moedas vão cair com ambas as caras para cima, ambas coroa, ou uma cara e uma coroa (conhecido como 'odds'). É tradicionalmente jogado no Anzac Day nos pubs e clubes em todo o lado na Australia, em parte para marcar uma experiencia partilhada com Diggers ao longo dos anos.",
     humanUse: "(Amount)"
 };
