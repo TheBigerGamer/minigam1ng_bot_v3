664fbbba800846ec72b60552769eb3af764727e5
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-02-21 18:05:16 +0000

    Update and rename exchange.js to transferir.js

--- commands/Economy/transferir.js
+++ commands/Economy/transferir.js
@@ -6,15 +6,15 @@ exports.run = async (client, msg, [user, credit]) => {
     if (data.valid === false) { return; }
     user = data.user[0];
 
-    if (user.id === msg.author.id) { return msg.channel.send("Why are you trying to exchange credits to yourself? I doubt you are that lonely in life."); }
+    if (user.id === msg.author.id) { return msg.channel.send("Porque é que estás a tentar transferir dinheiro para ti? Acho que estás sozinho na vida."); }
 
     db.get(`SELECT credits FROM scores WHERE userId = "${msg.author.id}"`, [], (err, row) => {
         if (err) { return console.log(err); }
-        if (!row) { return msg.reply("You haven't signed up and received your credits yet! D: Use `m~daily` (Using default prefix) to earn your first amount of credits."); } 
-        if (row.credits < credit) { return msg.reply("You don't have that many credits, baka!"); }
+        if (!row) { return msg.reply("Tu ainda não te registaste e recebeste os teus créditos! D: Usa `//daily` (Prefixo original) para ganhares os teus primeiros créditos."); } 
+        if (row.credits < credit) { return msg.reply("Não tens créditos suficientes, ò pá!"); }
             
         db.get(`SELECT credits FROM scores WHERE userId = "${user.id}"`, [], (err, row) => {
-            if (!row) { return msg.reply("That user has not gotten their first daily yet!"); }
+            if (!row) { return msg.reply("Esse player aninda não reclamou os seus primeiros créditos!"); }
             db.run(`UPDATE scores SET credits = ${Number(row.credits) + credit} WHERE userId = ${user.id}`);
         });
 
@@ -22,7 +22,7 @@ exports.run = async (client, msg, [user, credit]) => {
     });
     db.close();
 
-    msg.reply(`You have given ${credit} credits to ${user.prefered}`);
+    msg.reply(`Tu deste ${credit} créditos ao player: ${user.prefered}`);
 };
 
 exports.conf = {
@@ -36,8 +36,8 @@ exports.conf = {
 };
   
 exports.help = {
-    name: "exchange",
-    description: "Give someone some of your credits.",
+    name: "transferir",
+    description: "Dá a alguém alguns créditos teus.",
     usage: "[user:str] [credit:int]",
     usageDelim: " ",
     humanUse: "(Required: User)_(Required: Credit)"
