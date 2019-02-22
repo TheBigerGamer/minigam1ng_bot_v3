3e5aa56f4f619f6ed71adad6bac0fe1d285f0b52
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-02-22 23:02:02 +0000

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,5 +4,5 @@ exports.run = (client, guild) => {
     if (client.settings.guilds.schema.welcomeChannel === null) { var channel = client.funcs.defaultChannel(client, guild.guild); } 
     else { var channel = guild.guild.channels.find("id", guild.guild.settings.welcomeChannel); }
 
-    client.channels.get(channel.id).send(`${guild.user.username} has left the server. Good luck out there!`); 
+    client.channels.get(channel.id).send(`${guild.user.username} saiu do server. Boa sorte lá fora!`); 
 };
