e86d668ff40afa47f7621e526db702af96afc310
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-02-22 15:20:52 +0000

    Update and rename report.js to reporte.js

--- commands/General/reporte.js
+++ commands/General/reporte.js
@@ -6,30 +6,30 @@ exports.run = async (client, msg) => {
     const reports = [];
     
     const reportTypes = {
-        issue: ["Issue", 0xFF0000],
+        issue: ["Problema", 0xFF0000],
         bug: ["Bug", 0xFFFF00],
-        improvement: ["Improvement", 0xFFA500],
-        suggestion: ["Suggestion", 0xFFA500],
-        complaint: ["Complaint", 0xDB3E17],
-        todo: ["Todo", 0x04d5fd]
+        improvement: ["Melhoramento", 0xFFA500],
+        suggestion: ["Sugestão", 0xFFA500],
+        complaint: ["Complemento", 0xDB3E17],
+        todo: ["A fazer", 0x04d5fd]
     };
 
     const text = [
-        "Alright. Let's get to the point. First question: What kind of issue is this?\nPlease answer `issue`, `bug`, `complaint`, `suggestion`, or `improvement`",
+        "Muito bem. Sejamos diretos. 1ª Quetão: Que tipo de problema é esse?\nPor favor responde `issue`, `bug`, `complaint`, `suggestion`, ou `improvement`",
         "Next: Please provide a decently sized message explaining the item."
     ];
 
     const issue = [
-        "It seems you have timed out with making a report. When you are ready, feel free to try again!",
-        "You didn't provide me with a description in time. I recommend either making your report shorter or copy and pasting so that you don't have to try and type it so quickly."
+        "Parece que pensaste demasiado tempo. Quando tiveres pronto, tenta outra vez!",
+        "Dá-me uma descrição a tempo. Recomendo esncurtares a descrição ou copia-la e cola-la, para não tentares escrever muito rápidamente."
     ];
 
-    await msg.reply("I'm going to be asking a couple of questions so I'll be taking this into the DMs.");
+    await msg.reply("Vou-te perguntar algumas coisas no privado.");
     await msg.author.send(text[0]).then(() => {
         msg.author.dmChannel.awaitMessages(m => m.content, { max: 1, time: 160000, errors: ["time"], }).then((collected) => {
             var type = collected.first().content;
-            if (type.toLowerCase() !== reportTypes[type.toLowerCase()][0].toLowerCase()) { return msg.author.send("You have provided me with an invalid type of issue. Please try again with a valid one."); }
-            if (reportTypes[type.toLowerCase()][0] === "Todo" && msg.author.id !== client.owner.id) { return msg.author.send("You are not able to send todo reports. Only the bot owner can."); }
+            if (type.toLowerCase() !== reportTypes[type.toLowerCase()][0].toLowerCase()) { return msg.author.send("Deste-me um tipo de problema desconhecido. Tenta outra vez com um tipo válido."); }
+            if (reportTypes[type.toLowerCase()][0] === "Todo" && msg.author.id !== client.owner.id) { return msg.author.send("Não podes mandar reportes do tipo 'a fazer'. Apenas o Bot Manteiner e os MASTER podem."); }
             
             reports.push(reportTypes[type.toLowerCase()]);
             msg.author.send(text[1]).then(() => {
@@ -41,15 +41,15 @@ exports.run = async (client, msg) => {
                         const embed = new client.methods.Embed()
                             .setTimestamp()
                             .setColor(reports[0][1])
-                            .setTitle(`${reports[0][0]} Report: ${row.reportNumber}`)
+                            .setTitle(`${reports[0][0]} Reporte: ${row.reportNumber}`)
                             .setDescription(reports[1])
-                            .setFooter(`Reported by: ${msg.author.tag} from ${msg.channel.guild.name}`, msg.author.displayAvatarURL());
+                            .setFooter(`Reportado por: ${msg.author.tag} de ${msg.channel.guild.name}`, msg.author.displayAvatarURL());
     
                         const DMembed = new client.methods.Embed()
                             .setColor(0x00AE86)
                             .setTimestamp()
-                            .setDescription(`**Report number:** ${row.reportNumber} \n**Issue:** ${reports[1]} 
-                            \nYour report has been sent! Any more questions, please ask Butterstroke#7150!`);
+                            .setDescription(`**Número de Reporte:** ${row.reportNumber} \n**Problema:** ${reports[1]} 
+                            \nO teu reporte foi enviado! Mais alguma questão, pergunte aos MASTERs ou a DarkenLight Mage#2401!`);
    
                         db.run(`UPDATE stats SET reportNumber = ${row.reportNumber + 1} WHERE statName ="general"`); 
                         client.channels.get(reportChannel).send({embed});
@@ -71,8 +71,8 @@ exports.conf = {
 };
     
 exports.help = {
-    name: "report",
-    description: "File a report to the bot developer. (ie: Bug, issue, complaint)",
+    name: "reporte",
+    description: "Faz um reporte para o desenvolvedor do bot. (ie: Bug, problema, complemento)",
     usage: "",
-    extendedHelp: "Margarine will be sliding into your DMs for a few questions. Be sure to have DMs open and ready to answer some questions!"
+    extendedHelp: "Winston vai-te perguntar algumas coisa no privado. Verifica se tens o privado aberto e pronto para responderes!"
 };
