0ac23c5acf5d82f32f1f60a4b79258969d05a800
TheBigerGamer <39341546+TheBigerGamer@users.noreply.github.com>
2019-02-22 21:59:16 +0000

    Update volume.js

--- commands/Music/volume.js
+++ commands/Music/volume.js
@@ -1,15 +1,15 @@
 exports.run = async (client, msg, [volume]) => {
-  if (!msg.guild.voiceConnection) { throw "I am not connected in a voice channel, please add some songs to the mix first!"; }
+  if (!msg.guild.voiceConnection) { throw "Não estou conectado a um canal de voz, por favor dá-me algumas músicas para misturar primeiro!"; }
   const handler = client.queue.get(msg.guild.id);
-  if (!handler || handler.playing === false) { throw "Kind of hard to adjust the volume if I am not playing music."; }
+  if (!handler || handler.playing === false) { throw "É dificil ajustar o volume se eu não estou a tocar nada..."; }
 
   const dispatcher = msg.guild.voiceConnection.dispatcher;
 
   if (!volume) { return msg.send(`📢 Volume: ${Math.round(dispatcher.volume * 50)}%`); }
-  if (volume === 0) { return msg.send("You might as well mute me if you don't want any noise."); }
-  if (volume > 100) { return msg.send("100% is the max. You jsut can't have 110% with this bot."); }
+  if (volume === 0) { return msg.send("Se não me queres ouvir, mais vale me calares."); }
+  if (volume > 100) { return msg.send("100% é o máximo. Tu não podes dar 110% com este bot."); }
 
-  var emote = (volume < (dispatcher.volume * 50)) ? ["🔉", "Decreasing"] : ["🔊", "Increasing"];
+  var emote = (volume < (dispatcher.volume * 50)) ? ["🔉", "Diminuindo"] : ["🔊", "Aumentando"];
 
   dispatcher.setVolume(Math.min(volume) / 50, 2);
   msg.send(`${emote[0]} ${emote[1]} the volume! Volume: ${Math.round(dispatcher.volume * 50)}%`);
@@ -25,6 +25,6 @@ exports.conf = {
 
 exports.help = {
   name: "volume",
-  description: "Manage the volume for current song.",
+  description: "Ajusta o volume das músicas.",
   usage: "[volume:int]"
 };
